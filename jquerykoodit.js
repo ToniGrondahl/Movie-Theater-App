063524a100e6b730811b12a6b9e00c4401c2a531
ToniGrondahl <toni.grondahl@outlook.com>
2022-12-02 01:16:36 +0200

    updates

--- jquerykoodit.js
+++ jquerykoodit.js
@@ -1,82 +1,131 @@
-//Kun body latautuu se hakee finnkinon teatterit ja sijoittaa ne select listaan
-function loadAreas() {
-    //Lähettää datapyynnön 
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "https://www.finnkino.fi/xml/TheatreAreas/", true); //luodaan yhteys xml tiedostoon 
-    xmlhttp.send();
-  
-    //tarkistetaan että kaikki on ok
-    xmlhttp.onreadystatechange=function() { 
-      if(xmlhttp.readyState == 4 && xmlhttp.status==200) {
+$(document).ready(function(){         //jquery animation that works after the page has loaded all the content
+  $(".header").mouseenter(function(){        
+    $(".header").animate({letterSpacing: "+=10px"});
+  });
+$(".header").mouseleave(function(){         //animating the welcome text header by changing the letter spacing when the mouse enters the element.
+  $(".header").animate({letterSpacing: "-=10px"});
+  });
+});
 
-        //Tallenna vastauksen data muuttujaan käsittelyn helpottamiseksi
-        let xmlDoc = xmlhttp.responseXML;
-
-        //Käytetään "getElementsByTagName" jotta saadaan teatteri nimet ja id:eet 
-        var theatreNames = xmlDoc.getElementsByTagName("Name");
-        var theatreIDs = xmlDoc.getElementsByTagName("ID");
-  
-        //Looppi array listan läpi
-        for(var i = 0; i < theatreNames.length; i++) {
-
-          //Saadaan tekstiä xml tiedostoista
-          var theatreText = theatreNames[i].innerHTML;
-          var theatreID = theatreIDs[i].innerHTML;
-  
-          //Luo uuden option select listalle ja antaa arvoksi theatreID xml tiedostosta
-          document.getElementById("theatreList").innerHTML +=  '<option value = ' + theatreID + '>' + theatreText + '</option>';
-        }
-      }
+  //.ready tarkistaa, että DOM -sisältö on kokonaan valmis käsiteltäväksi
+$(document).ready(function() {
+  //kutsutaan .ajaxilla XML sivuston dataa
+  $.ajax({
+    url: "https://www.finnkino.fi/xml/TheatreAreas/",
+    type: "GET",
+    dataType: "html",
+    success: function(xml) {
+      //jos data on kunnossa (success), lähtee alla oleva fuktio toimintaan
+      //etsitään tag TheatreArea xml tiedostosta .each loopilla
+        $(xml).find('TheatreArea').each(function() {
+          //haetaan jokainen tag theatrearea ja niiden sisältä 'name' ja 'id'
+          var theatreText = $(this).find('Name').text();
+          var theatreID = $(this).find('ID').text();
+          //tulostetaan select tauluun sisällöt
+          $("#theatreList").append('<option value = ' + theatreID + '>' + theatreText + '</option>');
+        });
     }
-  }
+  });
+});
 
-// funktio joka hakee finnkinon sivulta sen teatterin elokuva aikataulut minkä käyttäjä valitsee
-function loadSchedule() {
+/* Kun käyttäjä valitsee teatterin niin se kutsuu tätä funktiota
+   joka hakee finnkinon sivuilta kyseisen teatterin elokuva aikataulun */
+$("#theatreList").change(function(){
+  $("#list").text("");
+  var id = $("#theatreList").val();
+  $("#userInput").css("display", "block");
 
-    // tyhjentää listan jotta olemassaolevan datan päälle ei tulostu uutta dataa 
-    document.getElementById("list").innerHTML = "";
-    
-    //Saa valitun teatterin id:een 
-    let id = document.getElementById("theatreList").value;
-    
-    //Lähettää pyynnön, käyttää sen teatterin id:eetä jolta haluaa saada dataa
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=" + id, true);
-    xmlhttp.send()
+  //Elokuvan tietojen ajax kutsu
+  $.ajax({
+    url: "https://www.finnkino.fi/xml/Schedule/?area=" + id,
+    type: "GET",
+    dataType: "html",
+    success: function(xml) {
+      //haetaan xml tiedot looppia hyödyntäen
+        $(xml).find('Show').each(function() {
+          //tallennetaan eri tiedot muuttujiin
+          var imageURL = '<img class="images" src="' + $(this).find('EventSmallImagePortrait').text() + '">';
+          var movie = $(this).find('Title').text();
+          var Genre = $(this).find('Genres').text();
+          var Schedule = $(this).find('dttmShowStart').text();
+          var Duration = $(this).find('LengthInMinutes').text();
+          var place = $(this).find("TheatreAuditorium").text();  
 
-    xmlhttp.onreadystatechange = function() {
-        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) { 
-        let xmlDoc = xmlhttp.responseXML; //varastoidaan xml data muuttujaan jotta sitä voidaan käyttää myöhemmin
-    
-        let movie = xmlDoc.getElementsByTagName("Title"); //xml tiedostosta saatu data asetetaan muuttujiin ja ne näytetään sivulla
-        let genre = xmlDoc.getElementsByTagName("Genres");
-        let picture = xmlDoc.getElementsByTagName("EventSmallImagePortrait");
-        let timetable = xmlDoc.getElementsByTagName("dttmShowStart");
-        let lengthmin = xmlDoc.getElementsByTagName("LengthInMinutes");
-        let place = xmlDoc.getElementsByTagName("TheatreAuditorium");   
-    
-        let add = "<table id='movie-list'>"; //Taulukon alku
-        add+= "<tr><th></th> <th>Elokuva</th><th>Genre</th> <th>Kesto</th><th>Alkamisaika</th> <th>Sali</th>"; //Määritellään taulukon otsikot 
-    
-        for (let i=0; i < movie.length; i++){ //loopataan data yhdestä teatterista jotta joka rivillä on tietoa yhdestä elokuvasta ja näytösajasta 
-    
-        let time = new Date(timetable[i].innerHTML);  //muotoillaan näytösajan data merkkijonosta päivämääräksi
-        add += '<tr>'; //lisätään rivejä taulukkoon 
-        add += '<td><img src="'+picture[i].innerHTML+'"></td>'; //lisätään kuva taulukon sisään 
-        add += '<td class="movietitle">' + movie[i].innerHTML + '</td>';
-        add += '<td>' + genre[i].innerHTML + '</td>';
-        add += '<td>' + lengthmin[i].innerHTML+ " min " +'</td>';
-        add += '<td>' + time.toLocaleTimeString([], {hour: "2-digit", minute:"2-digit"})+'</td>'; // muutetaan päivämäärän formaattia näyttääkseen vain kellonajan
-        add += '<td>' + place[i].innerHTML+'</td>';
-        add += '</tr>';
-    
-        }
-        // Taulukon loppu
-        add+="</table>";
-    
-        // Asetetaan taulukko "moviedata" diviin 
-        document.getElementById("moviedata").innerHTML = add;
-        document.getElementById("topbutton").style.display=""; 
-                }
-            }
-        }
+          //parsitaan xml:stä vain tarvittavat aikataulutiedot
+          var time = Schedule.slice(11, 16);
+
+         //lopullinen tulostus tauluun
+         $("#list").hide();
+          $("#list").append('<tr><td>'+ imageURL + '</td><th>' + movie + '<th>' + Genre + '</th><th>' + Duration + " min <th><th>" + time + '<th> <th>' + place + '</td>');
+          $("#list").fadeIn(400);
+        });
+    }
+  });
+});
+
+//Erillisen hakukentän funktiot
+$("#userInput").keyup(function(){
+  //Declare variables
+ var input = $("#userInput").val();
+ var filter = input.toUpperCase();
+ var table = $("#list");
+ // Loop through all list items, and hide those who don't match the search query
+ $(table).find('tr').each(function() {
+   var tdText = $(this).text();
+   if(tdText.toUpperCase().indexOf(filter) > -1 ) {
+     $(this).fadeIn(1000);
+   } else {
+     $(this).fadeOut(1000);
+   }
+ });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
